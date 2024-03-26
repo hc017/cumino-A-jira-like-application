384108af820b270265faf21c3dfa01a216f75996
hc <harshalchavan170@gmail.com>
2024-03-27 02:04:47 +0530

    Banner 2 Added

--- src/Pages/Home/ThirdSection/ThirdSection.jsx
+++ src/Pages/Home/ThirdSection/ThirdSection.jsx
@@ -5,7 +5,51 @@ const ThirdSection = () => {
   return (
     <div className="TH_Component">
       <div className="TH_innerComponent">
-        
+        <div className="TH_upper">
+          <p className="TH_title">
+            Facts only <br /> No more hunting for info <br /> This app keeps it
+            real
+          </p>
+        </div>
+        <div className="TH_lower">
+          <div className="TH_lower_steps">
+            <div className="TH_steps_box">
+              <div className="TH_box_circle">1</div>
+              <p className="TH_box_headline">Map Out Your Quest</p>
+              <p className="TH_box_subheadline">
+                Map out your project like a boss. This app got your back
+              </p>
+            </div>
+            <div className="TH_steps_box">
+              <div className="TH_box_circle">2</div>
+              <p className="TH_box_headline">Level Up Your Grind</p>
+              <p className="TH_box_subheadline">
+              Stay on fleek with your progress. No stress, just success
+              </p>
+            </div>
+            <div className="TH_steps_box">
+              <div className="TH_box_circle">3</div>
+              <p className="TH_box_headline">Ship It, Fam</p>
+              <p className="TH_box_subheadline">
+              Launch your project like a pro. No glitches, all wins.
+              </p>
+            </div>
+            <div className="TH_steps_box">
+              <div className="TH_box_circle">4</div>
+              <p className="TH_box_headline">Spill the Tea <br /> on Your Progress</p>
+              <p className="TH_box_subheadline">
+              Get the tea ☕ on your project's performance. Easy to understand, no lies
+              </p>
+            </div>
+            <div className="TH_steps_box">
+              <div className="TH_box_circle">1</div>
+              <p className="TH_box_headline">Auto-Pilot Activated</p>
+              <p className="TH_box_subheadline">
+              Put your project on autopilot. This app's got the magic touch
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
